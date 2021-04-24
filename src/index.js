0eede5f24a61c420ab56073d2d4496b6092d4c09
Alex576 <sasa.com630@gmail.com>
2021-04-24 23:44:09 +0300

    correct with linter

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const user = {
     city: 'Odessa',
 };
 const userNew = {};
-
+const one = -1;
+const zero = 0;
 //Copy an object to another object
-function cloneObj(obj){
-    for(const key in obj){
+function cloneObj(obj) {
+    for (const key in obj) {
         userNew[key] = obj[key];
     }
     return userNew;
@@ -15,33 +16,33 @@ function cloneObj(obj){
 cloneObj(user);
 
 //Return a string of values
-let stringValues = '';
-function getValues(obj,seperator){
-    for(const key in obj){
+function getValues(obj, seperator) {
+    let stringValues = '';
+    for (const key in obj) {
         stringValues += obj[key] + seperator;
     }
     //Remoted the last seperator
-    stringValues = stringValues.slice(0, -1);
+    stringValues = stringValues.slice(zero, one);
     return stringValues;
 }
 getValues(user, '_');
 
 //Return a string of keys
 let stringKeys = '';
-function getKeys(obj,seperator){
-    for(const key in obj){
+function getKeys(obj, seperator) {
+    for (const key in obj) {
         stringKeys += key + seperator;
     }
     //Remoted the last seperator
-    stringKeys = stringKeys.slice(0, -1);
+    stringKeys = stringKeys.slice(zero, one);
     return stringKeys;
 }
 getKeys(user, '_');
 
 //Return a string of objects
 let stringObjects = '';
-function getEntries(obj){
-    for(const key in obj){
+function getEntries(obj) {
+    for (const key in obj) {
         stringObjects += key + ': ' + obj[key] + '\n';
     }
     return stringObjects;
