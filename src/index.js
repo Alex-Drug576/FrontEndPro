3dd78216a4d9dfd3a8ee273e58354f684513e8b1
Alex576 <sasa.com630@gmail.com>
2021-04-25 15:34:34 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-// //function get variables and sum them
-// const zero = 0;
-// function sum(num) {
-//     let sumVariables = num;
-//     sum = function () {
-//         const firstVariable = arguments[zero] ? arguments[zero] : zero;
-//         return sumVariables += firstVariable;
-//     };
-//     return sum();
-// };
-// //Obj that can sum variables with step and method that can reset variable
-// function step(start, step) {
-//     let count = start;
-//     this.step1 = function () {
-//         return count += step;
-//     };
-// };
+// function getResultAll(operation) {
+//     let sum = 1;
+//     for (let i = 1; arguments.length > i; i++) {
+//         if (arguments[i] == Number && operation == '+' || '/' || '-' || '*') {
+//             switch (operation) {
+//                 case '+':
+//                     sum = sum + arguments[i];
+//                     break;
+//                 case '-':
+//                     sum = sum - arguments[i];
+//                     break;
+//                 case '/':
+//                     sum = sum / arguments[i];
+//                     break;
+//                 case '*':
+//                     sum = sum * arguments[i];
+//                     break;
+//             }
+//         } else return 0;
+//     }
+//     return sum;
+// }
+// console.log(getResultAll('+', 4, 3));
+// function doo() {
+//     this.step = function () {
+
+//     }
+// }
+
+
+
+
+
+
+
