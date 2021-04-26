e2cbb72a9462fa08f7c091bef386989265cf0386
Alex576 <sasa.com630@gmail.com>
2021-04-26 17:30:26 +0300

    correct

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-// //function get variables and sum them
-// const zero = 0;
-// function sum(num) {
-//     let sumVariables = num;
-//     sum = function () {
-//         const firstVariable = arguments[zero] ? arguments[zero] : zero;
-//         return sumVariables += firstVariable;
-//     };
-//     return sum();
-// };
-// //Obj that can sum variables with step and method that can reset variable
-// function step(start, step) {
-//     let count = start;
-//     this.step1 = function () {
-//         return count += step;
-//     };
-// };
+//function get variables and sum them
+const five = 5;
+const ten = 10;
+function sum() {
+    let sumVariables = 0;
+    return function (value) {
+        const result = value + sumVariables;
+        sumVariables += value;
+        return result;
+    };
+}
+const link = sum();
+link(five);
+//Obj that can sum variables with step and method that can reset variable
+function counter(start, step) {
+    const count = start;
+    let sumStep = 0;
+    return {
+        get count() {
+            const result = count + sumStep;
+            sumStep += step;
+            return result;
+        },
+        set count(reset) {
+            sumStep = reset;
+        },
+    };
+}
+const fit = counter(ten, five);
+fit.count();
