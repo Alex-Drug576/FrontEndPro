a82bcfb93d10d2b0377087b89a74300c676da27e
Alex576 <sasa.com630@gmail.com>
2021-04-29 16:19:29 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const foo = (fn, context) => () =>
-    fn.apply(context);
-function goo() {
-    return `Hello, ${this.name} ${this.surname}`;
+const foo = (fn, context, ...reset) => (...args) =>
+    fn.apply(context, [...reset, ...args]);
+function goo(context) {
+    return `Hello, ${this.name} ${this.surname} ${context}`;
 }
 const lo = {
     name: 'alex',
     surname: 'Mnogitskiy',
 };
 const ho = foo(goo, lo);
-ho();
+ho('!');
 
